e08d6c08740b59dee5be4f207ac793eda823f191
VHHBS <44783931+VHHBS@users.noreply.github.com>
2019-09-26 18:20:28 -0500

    Add Basement
    Test manually adding a scene.

--- data.js
+++ data.js
@@ -539,6 +539,33 @@ var APP_DATA = {
       },
       "linkHotspots": [],
       "infoHotspots": []
+    },
+	    {
+      "id": "20-basement",
+      "name": "Basement",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.1834767720162187,
+        "pitch": 0.05311496219149703,
+        "fov": 1.4261791587875234
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
     }
   ],
   "name": "Simon Remodel",
